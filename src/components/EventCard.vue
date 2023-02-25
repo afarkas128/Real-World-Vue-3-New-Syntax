8b58b3ac9a6d1b2d9a817a42919b1b7c192cbb93
Attila Farkas <attila@webgurus.biz>
2023-02-25 10:44:50 +0200

    Added dynamic routing with routerlink to the single event

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,11 +8,16 @@ defineProps({
 </script>
 
 <template>
-  <div class="event-card">
-    <h2>{{ event.title }}</h2>
-    <span>@{{ event.time }} on {{ event.date }} at {{ event.location }}</span>
-    <p>{{ event.title }} is organized by {{ event.organizer }}</p>
-  </div>
+  <RouterLink
+    class="event-link"
+    :to="{ name: 'event-details', params: { id: event.id } }"
+  >
+    <div class="event-card">
+      <h2>{{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }} at {{ event.location }}</span>
+      <p>{{ event.title }} is organized by {{ event.organizer }}</p>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
@@ -27,5 +32,9 @@ defineProps({
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.event-link {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,16 +1,21 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/afarkas128/Real-World-Vue-3-New-Syntax/events',,
+  baseURL:
+    'https://my-json-server.typicode.com/afarkas128/Real-World-Vue-3-New-Syntax',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 })
 
 export default {
   getEvents() {
     return apiClient.get('/events')
-  }
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
 }
